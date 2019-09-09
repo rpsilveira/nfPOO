484d8cdb2b27440ce206e384b6b3a4ae98cb5399
Reinaldo Silveira <reinaldopsilveira@gmail.com>
2019-09-09 09:56:18 -0300

    início do projeto

---
+++ projNF.dpr
@@ -0,0 +1,14 @@
+program projNF;
+
+uses
+  Vcl.Forms,
+  U_Principal in 'U_Principal.pas' {F_Principal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TF_Principal, F_Principal);
+  Application.Run;
+end.
